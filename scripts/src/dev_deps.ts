dc6d7b6b30967a9dcd1e519d93184e54bab267e6
William Bergamin <wbergamin@slack-corp.com>
2024-12-10 11:24:15 -0500

    chore: update dependencies

--- scripts/src/dev_deps.ts
+++ scripts/src/dev_deps.ts
@@ -5,7 +5,7 @@ export {
   assertFalse,
   assertIsError,
   assertStringIncludes,
-} from "https://deno.land/std@0.152.0/testing/asserts.ts";
+} from "https://deno.land/std@0.224.0/assert/mod.ts";
 export { assert } from "https://deno.land/x/conditional_type_checks@1.0.6/mod.ts";
 export type {
   IsExact,
